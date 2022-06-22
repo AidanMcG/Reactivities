783f803eadb2cfb71cdb769cc1e6d7ba2d296ca9
AidanMcG <aidan.mcgiv@gmail.com>
2022-06-22 14:55:52 +0100

    Module8

--- client-app/src/App/Layout/App.tsx
+++ client-app/src/App/Layout/App.tsx
@@ -1,27 +1,34 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import NavBar from './NavBar';
 import { Container } from 'semantic-ui-react';
 import ActivityDashboard from '../../Features/Activities/Dashboard/ActivityDashboard';
-import LoadingComponent from './LoadingComponents';
-import { useStore } from '../Stores/store';
 import { observer } from 'mobx-react-lite';
+import { Route, useLocation } from 'react-router-dom';
+import HomePage from '../../Features/Activities/Home/HomePage';
+import ActivityForm from '../../Features/Activities/Form/ActivityForm';
+import ActivityDetails from '../../Features/Activities/Details/ActivityDetails';
 
 
 function App() {
-  const {activityStore} = useStore();
-
-  useEffect(() => {
-    activityStore.loadActivities();
-  }, [activityStore])
-
-  if (activityStore.loadingInitial) return <LoadingComponent content='Loading app'/>
+  const location= useLocation();
 
   return (
     <>
-      <NavBar/>
-      <Container style={{marginTop: '7em'}} >
-        <ActivityDashboard />
-      </Container>
+      <Route exact path='/' component={HomePage}/>
+      <Route
+        path = {'/(.+)'}
+        render={() => (
+          <>
+            <NavBar/>
+            <Container style={{marginTop: '7em'}} >
+              <Route exact path='/activities' component={ActivityDashboard}/>
+              <Route path='/activities/:id' component={ActivityDetails}/>
+              <Route key ={location.key} path={['/createActivity', '/manage/:id']} component={ActivityForm}/>
+              </Container>
+          </>
+        )}
+      />
+      
     </>
   );
 }
--- client-app/src/Features/Activities/Details/ActivityDetails.tsx
+++ client-app/src/Features/Activities/Details/ActivityDetails.tsx
@@ -1,16 +1,25 @@
-import React from 'react';
+import { observer } from 'mobx-react-lite';
+import React, { useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
 import { Button, Card, Image } from 'semantic-ui-react';
 import LoadingComponent from '../../../App/Layout/LoadingComponents';
 import { useStore } from '../../../App/Stores/store';
 
 
 
-export default function ActivityDetails(){
+export default observer(function ActivityDetails(){
 
     const {activityStore} = useStore();
-    const {selectedActivity: activity, openForm, cancelSelectedActivity} = activityStore;
+    const {selectedActivity: activity, loadActivity, loadingInitial} = activityStore;
+    const {id} = useParams<{id: string}>();
+    //const id = '6c1654c5-d4eb-43fe-be8c-6dc04d33d208';
+    //const {id} = useParams<{id: string}>;
 
-    if (!activity) return <LoadingComponent />;
+    useEffect(() => {
+        if (id) loadActivity(id);
+    }, [id, loadActivity]);
+
+    if (loadingInitial || !activity) return <LoadingComponent />;
 
     return (
         <Card fluid>
@@ -26,11 +35,11 @@ export default function ActivityDetails(){
             </Card.Content>
             <Card.Content extra>
                 <Button.Group widths='2'>
-                    <Button onClick={() => openForm(activity.id)} basic color='blue' content='Edit'/>
-                    <Button onClick={cancelSelectedActivity} basic color='grey' content='Cancel'/>
+                    <Button as={Link} to={`/manage/${activity.id}`} basic color='blue' content='Edit'/>
+                    <Button as={Link} to='/activities' basic color='grey' content='Cancel'/>
                 </Button.Group>
             </Card.Content>
         </Card>
         
     )
-}
+})
